c7ad288ad8a5d75dc468532675ba3616aef8c5d4
Carl Emil Elling <carlemilelling@hotmail.com>
2026-02-03 15:50:28 +0100

    change in buffer size to accomodate real gantry speed

--- Software refactor/LIPI-Scanning/src/gantry_utils/core.py
+++ Software refactor/LIPI-Scanning/src/gantry_utils/core.py
@@ -5,7 +5,7 @@ import os
 from . import gCodeHandler
 from src.camera_utils import imageAcquisition
 import datetime
-
+import math
 __all__ = ['get_ports', 'scan_continuous', "connect", "calibrate"]
 #TODO: Make Self Contained to avoid handling gantry context outside
 
@@ -65,7 +65,7 @@ def calibrate(gcode_handler):
     gcode_handler.wait()
     print("Gantry homed!")
 
-def scan_continuous(gcode_handler,context,savePath,frameRate,speed=5000):
+def scan_continuous(gcode_handler,context,savePath,frameRate,speed=5000,gantry_lengthx=2100,gantry_lengthy=2000):
     """
     Perform a continuous scan of a PV panel using a gantry system and save the acquired images.
     Parameters:
@@ -82,13 +82,15 @@ def scan_continuous(gcode_handler,context,savePath,frameRate,speed=5000):
     - The function waits for the gantry to complete its movement before finishing.
     - The scanned image is saved with a timestamp in the filename.
     """
-    #offsetBegin=500 #offset from the first edge of the gantry to end stops. This is the 0-point in real life
-    #offsetEnd=100 #offset from the last edge of the gantry to max travel of the axes
-    dist_travel=2000#(2500-offsetBegin-offsetEnd) #Distance with offset included
-    nImages=int(dist_travel/(speed/60)*frameRate)
-    bufferSize=nImages+400
+    offsetBegin=200
+    px=gantry_lengthx-offsetBegin #Effective length of gantry in the X direction
+    py=gantry_lengthy #Fixed length of gantry in the Y direction
+    speedx=px*speed/(math.sqrt(px**2+py**2)) #Calculate speed in the X direction
+    speedy=py*speed/(math.sqrt(px**2+py**2)) #Calculate speed in the Y direction
+    nImages=int(px/(speedx/60)*frameRate)
+    bufferSize=nImages+200
     gcode_handler.set_speed(speed) #set speed for both axes
     gcode_handler.set_position(200,0)
-    gcode_handler.set_position(dist_travel+100,dist_travel)
+    gcode_handler.set_position(gantry_lengthx,gantry_lengthy)
     imageAcquisition.acquireImage(context,bufferSize,frameRate,nImages,savePath, fileName=f"scan_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}")
     gcode_handler.wait()
