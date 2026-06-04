0310cee786d1b109587daee65f98ad4257bf8a3e
Carl Emil Elling <carlemilelling@hotmail.com>
2026-06-04 11:26:06 +0200

    Update auto_exposure.py

--- Software refactor/LIPI_Scanning/auto_exposure.py
+++ Software refactor/LIPI_Scanning/auto_exposure.py
@@ -1,3 +1,9 @@
+
+import sys
+import os
+fli_path = os.path.abspath(os.path.join(os.getenv('FLISDK_DIR'), "Python/lib"))
+if fli_path not in sys.path:
+    sys.path.append(fli_path)
 from src.camera_utils import cred3
 import matplotlib.pyplot as plt
 import numpy as np
