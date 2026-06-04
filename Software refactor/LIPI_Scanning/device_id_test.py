c11c8b45bce6021ae54cc89c3f1563219c3df9ed
JeperoJ <55749013+JeperoJ@users.noreply.github.com>
2026-06-04 10:05:10 +0200

    Update device_id_test.py

--- Software refactor/LIPI_Scanning/device_id_test.py
+++ Software refactor/LIPI_Scanning/device_id_test.py
@@ -2,4 +2,4 @@ import serial
 from serial.tools import list_ports
 ports = list_ports.comports()
 for port in ports:
-    print(port)
+    print(port.name, port.device, port.pid, port.vid, port.hwid, port.description, port.interface, port.location, port.manufacturer, port.product, port.serial_number)
