f26ef31a336b36576e33bf532eeb0ed938ce73fd
JeperoJ <55749013+JeperoJ@users.noreply.github.com>
2026-06-04 10:12:35 +0200

    Update device_id_test.py

--- Software refactor/LIPI_Scanning/device_id_test.py
+++ Software refactor/LIPI_Scanning/device_id_test.py
@@ -1,5 +1,6 @@
 import serial
 from serial.tools import list_ports
 ports = list_ports.comports()
-for port in ports:
-    print(port.name, port.device, port.pid, port.vid, port.hwid, port.description, port.interface, port.location, port.manufacturer, port.product, port.serial_number)
+with open('device_id.txt', 'w') as f:
+    for port in ports:
+        print(port.name, port.device, port.pid, port.vid, port.hwid, port.description, port.interface, port.location, port.manufacturer, port.product, port.serial_number, file=f)
