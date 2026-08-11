da008a524f3e7134bd4c7227e0765df03dfce923
JeperoJ <55749013+JeperoJ@users.noreply.github.com>
2026-08-11 11:42:12 +0200

    Update basic_bias.py

--- Software refactor/LIPI_Scanning/basic_bias.py
+++ Software refactor/LIPI_Scanning/basic_bias.py
@@ -1,4 +1,9 @@
-from lib import FliSdk_V2
+import os
+import sys
+fli_path = os.path.abspath(os.path.join(os.getenv('FLISDK_DIR'), "Python/lib"))
+if fli_path not in sys.path:
+    sys.path.append(fli_path)
+import FliSdk_V2
 
 context = FliSdk_V2.Init()
 FliSdk_V2.DetectGrabbers(context)
