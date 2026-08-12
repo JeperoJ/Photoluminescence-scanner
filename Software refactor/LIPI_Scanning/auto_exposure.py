6fa56ae78384747c9eccb31afd9ff57bc64353bb
JeperoJ <55749013+JeperoJ@users.noreply.github.com>
2026-08-12 16:00:04 +0200

    Hopefully fixed auto exposure
    Updated auto exposure, now using established numerical analysis methods. Untested currently. Potentially issues with how SciPy functions and device communication but we will have to see. Else, manually writing the method is a possibility, but is just a chore.

--- Software refactor/LIPI_Scanning/auto_exposure.py
+++ Software refactor/LIPI_Scanning/auto_exposure.py
@@ -1,6 +1,9 @@
 
 import sys
 import os
+
+from flet.controls import margin
+
 fli_path = os.path.abspath(os.path.join(os.getenv('FLISDK_DIR'), "Python/lib"))
 if fli_path not in sys.path:
     sys.path.append(fli_path)
@@ -10,35 +13,78 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tifffile
 import time
+import scipy.optimize as opt
+
+
+def get_brightness(images, x1, y1, x2, y2):
+    """
+    Calculates the brightness of an image or series of images.
+
+    Args:
+        images: Captured images
+        x1, y1, x2, y2: Area to calculate brightness in
+
+    Returns:
+        brightness: float
+    """
+
+    return np.max(images[y1:y2, x1:x2])
+
+
+def auto_expose(self: cred3.Cred3, target_distance, N=10, x1=2, y1=2, x2=-2, y2=-2):
+    """
+    Auto exposes camera using methods from numerical analysis implemented in SciPy
+
+    Args:
+        target_distance: Target distance from max pixel value (14 bit)
+        N: Images to capture for each iteration
+        x1, y1, x2, y2: Bounds area that is exposed for
+    """
+    bias_before = self.config["bias_type"]
+    self.configure(bias_type="Off")
+
+    brightness_max = 2**14-1
+    res, exposure_min, exposure_max = fs.FliCredThree.GetTintRange(self.context)
+    if not res:
+        print("Failed to get Tint range")
+        return None
+    print("Exposure range: ", exposure_min, exposure_max)
+    brightness_target = brightness_max - target_distance # Target Y value
+
+    def f(x):
+        self.configure(exposure=x)
+        images = self.get_images(N)
+        brightness = get_brightness(images, x1, y1, x2, y2)
+        return brightness - brightness_target
+
+    optimized_exposure = opt.toms748(f, exposure_min, exposure_max)
+
+    self.configure(bias_type = bias_before, exposure = optimized_exposure)
+    return None
+
+
 
 if __name__ == "__main__":
-    camera = cred3.Cred3()
-    camera.connect()
-    camera.configure(bias_type="Off", conversion_gain = "Medium", fps=300)
+    # camera = cred3.Cred3()
+    # camera.connect()
+    # camera.configure(bias_type="Off", conversion_gain = "Medium", fps=300, exposure=1/(300*100))
+    #
+    # plt.imshow(camera.get_images(1)[0][2:-2,2:-2], cmap="gray")
+    # plt.show()
 
-    exposures_N = 100
-    exposure_min = 1/1000
-    exposure_max = fs.FliCredThree.GetTintMax(camera.context)[1]
-    print(exposure_max)
-    exposures = np.linspace(exposure_min, exposure_max, exposures_N)
-    print("Exposures range: ", exposures)
+    target_distance = 1000
+    Y_t = 2**14-1-target_distance
+    max_exposure = 1/300
+    min_exposure = max_exposure / 10
 
-    images_N = 10
-    for bias in ["Off", "Adaptive"]:
-        for conv_gain in ["Low", "Medium", "High"]:
-            camera.configure(bias_type=bias, conversion_gain=conv_gain)
-            if bias == "Manual":
-                input("Enter to build bias")
-                camera.build_bias()
-                input("Enter to continue")
-            for exposure in exposures:
-                time.sleep(0.1)
-                camera.configure(exposure=exposure)
-                images = camera.get_images(images_N)
-                _expo_act = camera.config["exposure"]
-                tifffile.imwrite(f"Auto_expo_testing//{bias}_{conv_gain}_{_expo_act}.tiff", images, photometric="minisblack", imagej=True)
+    res = opt.toms748(lambda x: f(x)-Y_t, min_exposure, max_exposure)
+    print(res)
+    print(f(res))
 
+    #auto_expose(camera, 1000, 1, 10)
 
+    # plt.imshow(camera.get_images(1)[0][2:-2, 2:-2], cmap="gray")
+    # plt.show()
 
 
 
